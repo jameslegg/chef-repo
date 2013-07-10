2135ae980969b8288761b343247e04cd61b68048
James Legg <mail@jameslegg.co.uk>
2013-07-10 21:33:40 +0100

    my first chef modules

---
+++ cookbooks/sshd/recipes/default.rb
@@ -0,0 +1,20 @@
+#
+# Cookbook Name:: sshd
+# Recipe:: default
+#
+# Copyright 2013, James Legg
+#
+#
+config = data_bag_item('sshd', 'config')
+template "/etc/ssh/sshd_config" do
+    source     "sshd_config.erb"
+    mode       "0644"
+    variables(
+        :sshd_ports => config['ports'],
+        :sshd_chal_resp => config['chal_resp']
+)
+end
+
+service "ssh" do
+    action [ :restart ]
+end
